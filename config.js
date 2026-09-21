65fbe4a92b90860816e64ffef23043886b2d981a
JorgeLFA01 <iorch001@gmail.com>
2026-09-21 10:12:21 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
-let apiUrl = window.NEXAPRINT_CONFIG?.apiUrl || localStorage.getItem('nexa-url') || '';
-document.querySelector('#api-url').value = apiUrl;
+window.NEXAPRINT_CONFIG = {
+  apiUrl: 'https://script.google.com/macros/s/AKfycbwdwlgIloP78qPx3McA4w9uut6ZTiUd7hXXx94Cpnkxo4KCBsc0g_JJb5xAXvM0IvQ/exec'
+};
+
+document.querySelector('#api-url').value =
+  window.NEXAPRINT_CONFIG.apiUrl;
