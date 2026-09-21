d7a7f91e62733f3e2d3b9634962a45901af04c0b
JorgeLFA01 <iorch001@gmail.com>
2026-09-21 10:07:00 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// Pega únicamente la URL pública /exec. Nunca agregues claves a este archivo.
-window.NEXAPRINT_CONFIG = { apiUrl: 'https://script.google.com/macros/s/AKfycbwdwlgIloP78qPx3McA4w9uut6ZTiUd7hXXx94Cpnkxo4KCBsc0g_JJb5xAXvM0IvQ/exec' };
+let apiUrl = window.NEXAPRINT_CONFIG?.apiUrl || localStorage.getItem('nexa-url') || '';
+document.querySelector('#api-url').value = apiUrl;
